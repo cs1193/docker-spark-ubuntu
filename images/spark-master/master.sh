a7de4f9558aa8e1354cfeab819124425ae1cb12c
Chandresh Rajkumar Manonmani <cs1193@gmail.com>
2020-06-05 20:56:48 +0530

    fix: dev: master: corrected master hostname

--- images/spark-master/master.sh
+++ images/spark-master/master.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+export SPARK_MASTER_HOST=`hostname`
+
 spark-class org.apache.spark.deploy.master.Master --host $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
