21e6da3eee946b1ffb10ed68fc689324f08d2f6f
Chandresh Rajkumar Manonmani <cs1193@gmail.com>
2020-06-05 21:15:51 +0530

    fix: dev: master: added dev compose, master host updated

--- images/spark-master/master.sh
+++ images/spark-master/master.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export SPARK_MASTER_HOST=`hostname`
+export SPARK_MASTER_HOST='0.0.0.0'
 
 spark-class org.apache.spark.deploy.master.Master --host $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
